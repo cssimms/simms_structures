0ac7bedcc723eef69071b4d3394c1f2948094311
Charlie Simms <cssimms@gmail.com>
2016-05-21 11:13:56 -0700

    add dynamic array implementation

---
+++ dynamic_array/dynamic_array.rb
@@ -0,0 +1,76 @@
+require_relative "static_array"
+require 'byebug'
+
+class DynamicArray
+  attr_reader :length
+
+  def initialize(size = 8)
+    @store = StaticArray.new(size)
+    @length = 0
+    @capacity = size
+  end
+
+  # O(1)
+  def [](index)
+    raise 'index out of bounds' if index >= (@length)
+    @store[index]
+  end
+
+  # O(1)
+  def []=(index, value)
+    @store[index] = value
+  end
+
+  # O(1)
+  def pop
+    raise 'index out of bounds' if @length == 0
+    value = self[@length - 1]
+    @length -= 1
+    value
+  end
+
+  # O(1) ammortized; O(n) worst case. Variable because of the possible
+  # resize.
+  def push(value)
+    resize! if @length == @capacity
+    self[@length] = value
+    @length += 1
+  end
+
+  # O(n): has to shift over all the elements.
+  def shift
+    raise 'index out of bounds' if @length == 0
+    1.upto(@length - 1) do |idx|
+      self[idx - 1] = self[idx]
+    end
+    @length -= 1
+  end
+
+  # O(n): has to shift over all the elements.
+  def unshift(value)
+    resize! if @length == @capacity
+
+    (@length - 1).downto(0) do |idx|
+      self[idx + 1] = self[idx]
+    end if @length > 0
+    self[0] = value
+    @length += 1
+  end
+
+  protected
+  attr_accessor :capacity, :store
+  attr_writer :length
+
+  def check_index(index)
+    
+  end
+
+  # O(n): has to copy over all the elements to the new store.
+  def resize!
+    new_capacity = @capacity * 2
+    new_store = StaticArray.new(new_capacity)
+    @length.times { |idx| new_store[idx] = self[idx] }
+    @store = new_store
+    @capacity = new_capacity
+  end
+end
---
+++ dynamic_array/ring_buffer.rb
@@ -0,0 +1,80 @@
+require_relative 'static_array'
+require_relative 'dynamic_array'
+
+class RingBuffer
+  attr_reader :length
+
+  def initialize(size = 8)
+    @store = StaticArray.new(size)
+    @start_idx = 0
+    @length = 0
+    @capacity = 8
+  end
+
+  def relative_index(index)
+    (@start_idx + index) % @capacity
+  end
+
+  # O(1)
+  def [](index)
+    check_index(index)
+    @store[relative_index(index)]
+  end
+
+  # O(1)
+  def []=(index, value)
+    check_index(index)
+    @store[relative_index(index)] = value
+  end
+
+  # O(1)
+  def pop
+    check_index(0)
+    @length -= 1
+    @store[(@start_idx + @length) % @capacity]
+  end
+
+  # O(1) ammortized
+  def push(value)
+    resize! if @length == @capacity
+    @store[(@start_idx + @length) % @capacity] = value
+    @length += 1
+    nil
+    # debugger
+  end
+
+  # O(1)
+  def shift
+    check_index(0)
+    value = self[0]
+    @start_idx = (@start_idx + 1) % @capacity
+    @length -= 1
+    value
+  end
+
+  # O(1) ammortized
+  def unshift(value)
+    resize! if @length == @capacity
+    @start_idx = (@start_idx - 1) % @capacity
+    @store[@start_idx] = value
+    @length += 1
+  end
+
+  protected
+  attr_accessor :capacity, :start_idx, :store
+  attr_writer :length
+
+  def check_index(index)
+    raise 'index out of bounds' if index >= @length
+  end
+
+  def resize!
+    new_capacity = @capacity * 2
+    new_store = StaticArray.new(new_capacity)
+    @length.times { |idx| new_store[idx] = @store[relative_index(idx)]}
+
+    @store = new_store
+    @capacity = new_capacity
+    @start_idx = 0
+  end
+end
