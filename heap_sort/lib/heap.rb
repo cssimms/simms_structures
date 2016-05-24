f639b5156cdd2ff6bec237d72c9a746f917ba760
Charlie Simms <cssimms@gmail.com>
2016-05-24 14:39:37 -0700

    implement heap

--- heap_sort/lib/heap.rb
+++ heap_sort/lib/heap.rb
@@ -2,6 +2,7 @@ require 'byebug'
 
 class BinaryMinHeap
   def initialize(&prc)
+    @prc = prc
     @store = Array.new
   end
 
@@ -14,12 +15,17 @@ class BinaryMinHeap
   end
 
   def extract
-
+    last = @store.length - 1
+    @store[0], @store[last] = @store[last], @store[0]
+    output = @store.pop
+    BinaryMinHeap.heapify_down(@store, 0)
+    output
   end
 
 # change name to insert later
   def push(val)
-
+    @store.push(val)
+    BinaryMinHeap.heapify_up(@store, @store.length - 1)
   end
 
   protected
@@ -41,12 +47,32 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
 
+    child_idxs = BinaryMinHeap.child_indices(len, parent_idx)
+
+    if child_idxs.empty?
+      return array
+    elsif child_idxs.count == 1
+      target_child = child_idxs.first
+    else
+      if prc.call(array[child_idxs.first], array[child_idxs.last]) > -1
+        target_child = child_idxs.last
+      else
+        target_child = child_idxs.first
+      end
+    end
+
+    if prc.call(array[parent_idx], array[target_child]) > -1
+      array[parent_idx], array[target_child] = array[target_child], array[parent_idx]
+      BinaryMinHeap.heapify_down(array, target_child, len, &prc)
+    end
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
     return if child_idx == 0
-    prc = Proc.new { |a, b| a <=> b } unless prc
+    prc ||= Proc.new { |a, b| a <=> b }
 
     parent_idx = BinaryMinHeap.parent_index(child_idx)
     if prc.call(array[parent_idx], array[child_idx]) > -1
