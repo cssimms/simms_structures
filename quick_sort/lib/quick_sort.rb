282ecaec414b146b5f1c1d490b05bafc1d58cfb0
Charlie Simms <cssimms@gmail.com>
2016-05-25 08:58:53 -0700

    invalid implementation

--- quick_sort/lib/quick_sort.rb
+++ quick_sort/lib/quick_sort.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class QuickSort
   # Quick sort has average case time complexity O(nlogn), but worst
   # case O(n**2).
@@ -22,14 +23,41 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    return array if length < 2
+    prc ||= Proc.new { |a, b| a <=> b }
 
+    pivot_idx = QuickSort.partition(array, start, length, &prc)
+
+    left_length = pivot_idx - start
+    right_length = length - (left_length + 1)
+
+    sort2!(array, start, left_length, &prc)
+    sort2!(array, pivot_idx + 1, right_length, &prc)
+    array
   end
 
-  def self.partition(array, start, length, &prc)
+  def self.partition(array, start, length = array.length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
 
-    array.each do |el|
-      
+    first_right_idx = nil
+    (start + 1).upto(start + length - 1) do |idx|
+      case prc.call(array[start], array[idx])
+      when -1
+        first_right_idx ||= idx
+      when 1
+        if first_right_idx
+          array[first_right_idx], array[idx] = array[idx], array[first_right_idx]
+          first_right_idx += 1
+        end
+      end
     end
 
+    # swap pivot with last 'left' element
+    if first_right_idx
+      array[start], array[first_right_idx - 1] = array[first_right_idx - 1], array[start]
+      (first_right_idx - 1)
+    else
+      start
+    end
   end
 end
